56c9f48adf7be750dd5c47aa33a37cbbeec37d50
Alizade177 <ferid.elizade65@gmail.com>
2023-05-11 22:23:54 +0400

    init

---
+++ MyPortfolioProject/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolioProject.Models;
+using System.Data.SqlClient;
+using System.Diagnostics;
+
+namespace MyPortfolioProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        private  readonly IConfiguration _configuration;
+
+        public HomeController(IConfiguration configuration)
+        {
+            _configuration = configuration; 
+        }
+      public IActionResult Index() 
+      {
+            List<Skill> skills = new List<Skill>();
+            SqlConnection connection = null;
+            try
+            {
+                connection = new SqlConnection(_configuration.GetConnectionString("SqlServerConnection"));
+                SqlCommand command = new SqlCommand("SELECT * FROM Skills", connection);
+                connection.Open();
+                SqlDataReader dataReader = command.ExecuteReader();
+                while (dataReader.Read())
+                {
+                    skills.Add(new Skill()
+                    {
+                        Id = (int)dataReader["Id"],
+                        SkillName = (string)dataReader["SkillName"],
+                        SkillDescription = (string)dataReader["SkillDescription"],
+                        SkillValue = (int)dataReader["SkillValue"]
+
+                    });
+
+                }
+                
+            }
+            catch (Exception ex)
+            {
+            }
+            finally
+            {
+                connection.Close();
+            }
+           
+          
+             return View(skills);  
+      }
+    }
+}
---
+++ MyPortfolioProject/Models/Skill.cs
@@ -0,0 +1,11 @@
+﻿namespace MyPortfolioProject.Models
+{
+    public class Skill
+    {
+       public int Id { get; set; } 
+       public string SkillName { get; set; }    
+       public string SkillDescription { get; set;}
+       public int SkillValue { get; set;}
+
+    }
+}
